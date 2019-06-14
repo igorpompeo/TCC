018ec028fcf6fd39bd09c5735ba21b303e85f1c1
igorpompeo <pompbass@gmail.com>
2019-06-14 11:19:17 -0300

    Delete do que é desnecessário.

--- Lab.-Software-E-Projetos/Script_Alter_Table_Primary_Key.sql
+++
@@ -1,7 +0,0 @@
--- criação de pk
-alter table aluno add primary key (id);
-alter table professor add primary key (id);
-alter table materia add primary key (id);
-alter table curso add primary key (id);
-alter table prova add primary key (id);
-alter table tipo_prova add primary key (id);
